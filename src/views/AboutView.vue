78feb0036081fa3f26da0a958c99d6bd6d399a9f
Tran Quang Ha <114595165+haqt2003@users.noreply.github.com>
2024-10-11 09:41:38 +0700

    Update AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -163,6 +163,14 @@
     >
       Đặt hàng ngay
     </router-link>
+    <div class="mt-12 lg:mt-20 flex flex-wrap items-center">
+      <iframe
+        class="w-full h-[200px] sm:h-[350px] lg:h-[500px] rounded-lg"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.400197994391!2d105.7802760750299!3d20.976589180659747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acd2d99ec41f%3A0xc77a3683d6b47ae7!2zMTQwIMSQxrDhu51uZyBUcuG6p24gUGjDuiwgUC4gTeG7mSBMYW8sIEjDoCDEkMO0bmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1728614258873!5m2!1svi!2s"
+        allowfullscreen=""
+        loading="lazy"
+      ></iframe>
+    </div>
   </div>
   <footer-component />
 </template>
