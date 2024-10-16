c018f1dec3dc980f2a42537bfce3848b047928f1
Tran Ha <haqt2003@gmail.com>
2024-10-16 16:18:20 +0700

    policy

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -166,8 +166,7 @@
     <div class="mt-12 lg:mt-20 flex flex-wrap items-center">
       <iframe
         class="w-full h-[200px] sm:h-[350px] lg:h-[500px] rounded-lg"
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3151.835434509632!2d144.95373531531962!3d-37.81627997975114!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11b0b7%3A0x5045675218cee60!2sVictoria%20State%20Library!5e0!3m2!1sen!2sau!4v1632045472070!5m2!1sen!2sau"
-        style="border: 0"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3725.400197994391!2d105.7802760750299!3d20.976589180659747!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135acd2d99ec41f%3A0xc77a3683d6b47ae7!2zMTQwIMSQxrDhu51uZyBUcuG6p24gUGjDuiwgUC4gTeG7mSBMYW8sIEjDoCDEkMO0bmcsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1728614258873!5m2!1svi!2s"
         allowfullscreen=""
         loading="lazy"
       ></iframe>
