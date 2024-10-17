9010a55e6bc8894b35afb91b216b93d20b244551
Tran Ha <haqt2003@gmail.com>
2024-10-17 23:31:42 +0700

    update

--- src/views/PrivacyPolicy.vue
+++ src/views/PrivacyPolicy.vue
@@ -2,63 +2,61 @@
   <div class="w-full px-5 xl:px-20 py-10">
     <div class="w-full">
       <p class="text-center text-[32px] sm:text-[40px] font-bold">
-        Privacy Policy
+        Chính Sách Pháp Lý
       </p>
       <div class="w-full mt-10 h-[1px] bg-black"></div>
       <div class="space-y-[32px] mt-[24px]">
         <p class="text-justify">
-          The purpose of this policy is to define how The Best Records collects,
-          processes, stores, and protects personal and sensitive data to ensure
-          compliance with data protection laws and maintain the privacy and
-          confidentiality of individuals' data. This policy applies to all
-          employees, contractors, and third parties that interact with or
-          process data on behalf of The Best Records. It covers all data
-          collected, whether electronically or physically, and includes all
-          operational procedures related to data management. Personal data
-          refers to information that can identify an individual directly or
-          indirectly, such as name, address, email, or phone number. Sensitive
-          data requires additional protection and includes information such as
-          financial details, health records, racial or ethnic origin, and
-          religious beliefs. A data subject is any individual whose personal
-          data is collected, held, or processed by The Best Records. A data
-          processor is responsible for processing data on behalf of the data
-          controller, who determines the purposes and means of processing
-          personal data.
+          Chính sách này nhằm mục đích xác định cách The Best Records thu thập,
+          xử lý, lưu trữ và bảo vệ dữ liệu cá nhân và nhạy cảm để đảm bảo tuân
+          thủ các luật bảo vệ dữ liệu và duy trì quyền riêng tư cũng như tính
+          bảo mật của dữ liệu cá nhân. Chính sách này áp dụng cho tất cả nhân
+          viên, nhà thầu và bên thứ ba tương tác hoặc xử lý dữ liệu thay mặt cho
+          The Best Records. Nó bao gồm tất cả dữ liệu được thu thập, dù ở dạng
+          điện tử hay vật lý, và bao gồm tất cả các quy trình hoạt động liên
+          quan đến quản lý dữ liệu. Dữ liệu cá nhân đề cập đến thông tin có thể
+          xác định một cá nhân một cách trực tiếp hoặc gián tiếp, chẳng hạn như
+          tên, địa chỉ, email hoặc số điện thoại. Dữ liệu nhạy cảm yêu cầu bảo
+          vệ bổ sung và bao gồm thông tin như chi tiết tài chính, hồ sơ sức
+          khỏe, nguồn gốc chủng tộc hoặc dân tộc, và tín ngưỡng tôn giáo. Một
+          chủ thể dữ liệu là bất kỳ cá nhân nào có dữ liệu cá nhân được thu
+          thập, lưu giữ hoặc xử lý bởi The Best Records. Một người xử lý dữ liệu
+          có trách nhiệm xử lý dữ liệu thay mặt cho người kiểm soát dữ liệu,
+          người xác định mục đích và phương tiện xử lý dữ liệu cá nhân.
         </p>
 
         <p class="text-justify">
-          Data must be collected and processed lawfully, fairly, and in a
-          transparent manner. The purpose of data collection must be clearly
-          communicated to the data subject, and where necessary, their consent
-          must be obtained. Data should only be collected for specified,
-          explicit, and legitimate purposes, and it must not be further
-          processed in ways that are incompatible with these original purposes.
-          The principle of data minimization dictates that only the minimum
-          amount of personal data necessary for the intended purpose should be
-          collected and processed. Additionally, data accuracy is essential;
-          therefore, every effort should be made to ensure that personal data is
-          accurate, complete, and kept up to date. Inaccurate data must be
-          rectified or deleted promptly.
+          Dữ liệu phải được thu thập và xử lý một cách hợp pháp, công bằng và
+          minh bạch. Mục đích thu thập dữ liệu phải được thông báo rõ ràng đến
+          chủ thể dữ liệu, và khi cần thiết, phải được sự đồng ý của họ. Dữ liệu
+          chỉ nên được thu thập cho các mục đích đã được xác định rõ ràng, hợp
+          pháp và không được xử lý thêm theo những cách không tương thích với
+          các mục đích ban đầu này. Nguyên tắc tối thiểu hóa dữ liệu quy định
+          rằng chỉ nên thu thập và xử lý lượng dữ liệu cá nhân tối thiểu cần
+          thiết cho mục đích dự định. Thêm vào đó, độ chính xác của dữ liệu là
+          rất quan trọng; do đó, cần nỗ lực đảm bảo rằng dữ liệu cá nhân chính
+          xác, đầy đủ và được cập nhật kịp thời. Dữ liệu không chính xác phải
+          được sửa chữa hoặc xóa ngay lập tức.
         </p>
 
         <p class="text-justify">
-          Data should not be retained longer than necessary for the specified
-          purpose, in accordance with applicable legal or regulatory
-          requirements. Proper security measures must be in place to protect
-          data from unauthorized access, disclosure, alteration, and
-          destruction. These measures include encryption, access controls,
-          regular security assessments, and employee training. Furthermore, The
-          Best Records must ensure that any third parties or contractors that
-          process data on its behalf comply with this policy and the relevant
-          data protection laws. Regular audits and assessments will be conducted
-          to monitor compliance, and breaches or violations of this policy will
-          be met with appropriate corrective actions, which may include
-          penalties or disciplinary action. This policy will be reviewed
-          periodically and updated to reflect changes in data protection laws
-          and best practices. All employees and relevant parties are required to
-          adhere strictly to this policy, and any questions or concerns
-          regarding data protection should be directed to the Data Protection
-          Officer at The Best Records.
+          Dữ liệu không nên được lưu trữ lâu hơn mức cần thiết cho mục đích đã
+          chỉ định, phù hợp với các yêu cầu pháp lý hoặc quy định hiện hành. Các
+          biện pháp bảo mật thích hợp phải được áp dụng để bảo vệ dữ liệu khỏi
+          việc truy cập, tiết lộ, thay đổi và phá hủy trái phép. Những biện pháp
+          này bao gồm mã hóa, kiểm soát truy cập, đánh giá bảo mật định kỳ và
+          đào tạo nhân viên. Hơn nữa, The Best Records phải đảm bảo rằng bất kỳ
+          bên thứ ba hoặc nhà thầu nào xử lý dữ liệu thay mặt cho họ đều tuân
+          thủ chính sách này và các luật bảo vệ dữ liệu liên quan. Các cuộc kiểm
+          toán và đánh giá định kỳ sẽ được tiến hành để giám sát sự tuân thủ, và
+          các vi phạm hoặc sai sót của chính sách này sẽ được xử lý bằng các
+          biện pháp khắc phục thích hợp, có thể bao gồm các hình phạt hoặc hành
+          động kỷ luật. Chính sách này sẽ được xem xét định kỳ và cập nhật để
+          phản ánh những thay đổi trong luật bảo vệ dữ liệu và các phương pháp
+          tốt nhất. Tất cả nhân viên và các bên liên quan cần tuân thủ nghiêm
+          ngặt chính sách này, và mọi câu hỏi hoặc mối quan tâm liên quan đến
+          bảo vệ dữ liệu nên được gửi đến Nhân viên Bảo vệ Dữ liệu tại The Best
+          Records.
         </p>
       </div>
     </div>
